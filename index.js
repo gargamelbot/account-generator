e33609a046a2f66716234a7997a2067eb305ed52
nxxh <65509800+nxxh447@users.noreply.github.com>
2022-01-10 12:34:42 -0500

    better code

--- index.js
+++ index.js
@@ -1,163 +1,109 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const prefix = "/";
-var fs = require("fs");
-var lineReader = require("line-reader");
-var async = require("async");
+const lineReader = require("line-reader");
 const firstline = require("firstline");
+const Discord = require("discord.js");
+const os = require("os");
+const async = require("async");
+const fs = require("fs");
+const express = require('express');
+
+const bot = new Discord.Client();
 const generated = new Set();
-var os = require("os");
 
-var express = require('express');
-var app = express();
+let app = express();
+let prefix = "/";
 
-app.set('port', (process.env.PORT || 5000));
+let messageArray = message.content.split(" ");
+let args = messageArray.slice(1);
 
-//For avoidong Heroku $PORT error
-app.get('/', function (request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-bot.on("ready", () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
+
+app.set("port", (process.env.PORT || 5000));
+app.get("/", (request, response) => response.send("App is running")); //For avoidong Heroku $PORT error
+    .listen(app.get("port"), () => {
+        console.log(`App is running, server is listening on port: ${app.get('port')}`);
+    }
 });
 
-bot.on("message", message => {
+bot.on("ready", () => console.log(`Logged in as ${bot.user.tag}!`));
+bot.on("message", (message) => {
     if (message.channel.id === "Channel_ID") { //This will make the bot work only in that channel
         if (message.author.bot) return;
-        var command = message.content
+        let command = message.content
             .toLowerCase()
             .slice(prefix.length)
             .split(" ")[0];
-
-        if (command === "test") {
-            message.channel.send("Test done, bot is working");
-        }
-
+        if (command === "test") message.channel.send("Test done, bot is working");
         if (command === "gen") {
             if (generated.has(message.author.id)) {
-                message.channel.send(
-                    "Wait 15 minute before generating another account!. - " +
-                    message.author
-                );
+                message.reply(`Wait 15 minutes before generating another account! - ${message.author}`);
             } else {
+                const filePath = __dirname + "/" + args[0] + ".txt";
                 let messageArray = message.content.split(" ");
                 let args = messageArray.slice(1);
-                if (!args[0])
-                    return message.reply("Please, specify the service you want!");
-                var fs = require("fs");
-                const filePath = __dirname + "/" + args[0] + ".txt";
-
-                fs.readFile(filePath, function (err, data) {
+                
+                if (!args[0]) return message.reply("Please, specify the service you want!");
+                fs.readFile(filePath,(err, data) => {
                     if (!err) {
+                        let position = data.toString().indexOf("\n");
+                        let firstLine = data.split("\n")[0];
+                        
                         data = data.toString();
-                        var position = data.toString().indexOf("\n");
-                        var firstLine = data.split("\n")[0];
                         message.author.send(firstLine);
+                        
                         if (position != -1) {
                             data = data.substr(position + 1);
-                            fs.writeFile(filePath, data, function (err) {
+                            fs.writeFile(filePath, data, (err) => {
                                 const embed = {
                                     title: "Account Generated!",
                                     description: "Check your dm for the account's information!",
                                     color: 8519796,
                                     timestamp: "2019-04-04T14:16:26.398Z",
                                     footer: {
-                                        icon_url:
-                                            "https://cdn.discordapp.com/avatars/530778425540083723/7a05e4dd16825d47b6cdfb02b92d26a5.png",
+                                        icon_url: "https://cdn.discordapp.com/avatars/530778425540083723/7a05e4dd16825d47b6cdfb02b92d26a5.png",
                                         text: "Buy discord accounts from Mental#8106"
-                                    },
-                                    thumbnail: {
-                                        url:
-                                            "http://www.compartosanita.it/wp-content/uploads/2019/02/right.png"
-                                    },
+                                    }, thumbnail: { url:"http://www.compartosanita.it/wp-content/uploads/2019/02/right.png" },
                                     author: {
                                         name: "Account Generator",
                                         url: "https://discordapp.com",
                                         icon_url: bot.displayAvatarURL
-                                    },
-                                    fields: []
-                                };
-                                message.channel.send({ embed });
+                                    }, fields: []
+                                }; message.reply({ embed });
                                 generated.add(message.author.id);
-                                setTimeout(() => {
-                                    // Removes the user from the set after a minute
-                                    generated.delete(message.author.id);
-                                }, 150000);
+                                setTimeout(() => generated.delete(message.author.id), 150000); // Removes the user from the set after a minute
                                 if (err) {
                                     console.log(err);
                                 }
                             });
-                        } else {
-                            message.channel.send(
-                                "Sorry, there isn't any account available for that service!"
-                            );
-                        }
-                    } else {
-                        message.channel.send(
-                            "Sorry, that service doesn't exists on our database"
-                        );
-                    }
+                        } else message.reply("Sorry, there isn't any account available for that service!");
+                    } else message.reply("Sorry, that service doesn't exists on our database");
                 });
             }
-        }
-        else
-            if (command === "stats") {
-
-                message.channel.send(`Total users: ${bot.users.size}`)
-            }
-
+        } else if (command === "stats") message.reply(`Total users: ${bot.users.size}`)
         if (command === "add") {
-            if (!message.member.hasPermission("ADMINISTRATOR"))
-                return message.reply("Sorry, you can't do it, you are not an admin!");
-            var fs = require("fs");
-            let messageArray = message.content.split(" ");
-            let args = messageArray.slice(1);
-            var account = args[0]
-            var service = args[1]
             const filePath = __dirname + "/" + args[1] + ".txt";
-            fs.appendFile(filePath, os.EOL + args[0], function (err) {
+            let account = args[0]
+            let service = args[1]
+            
+            if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry, you can't do it, you are not an admin!");
+            fs.appendFile(filePath, os.EOL + args[0], (err) => {
                 if (err) return console.log(err);
-                message.channel.send("Done!")
+                message.reply("Done!")
             });
-
-
         }
+        
         if (command === "create") {
-            if (!message.member.hasPermission("ADMINISTRATOR"))
-                return message.reply("Sorry, you can't do it, you are not an admin!");
-            var fs = require("fs");
-            let messageArray = message.content.split(" ");
-            let args = messageArray.slice(1);
             const filePath = __dirname + "/" + args[0] + ".txt";
-            fs.writeFile(filePath, 'first:first', function (err) {
+            if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry, you can't do it, you are not an admin!");
+            fs.writeFile(filePath, "first:first", (err) => {
                 if (err) throw err;
                 message.channel.send("Done!")
             });
         }
+        
         if (command === "restock") {
-            let messageArray = message.content.split(" ");
-            let args = messageArray.slice(1);
-            if (!message.member.hasPermission("ADMINISTRATOR"))
-                return message.reply("Sorry, you can't do it, you are not an admin!");
-            if (!args[0])
-                return message.reply(
-                    "Please, specify the service you want to restock!"
-                );
-            message.channel.send(
-                "@everyone " +
-                "**" +
-                args[0] +
-                "**" +
-                " has been restocked by " +
-                "<@" +
-                message.author.id +
-                ">"
-            );
+            let difArg = "**";
+            if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry, you can't do it, you are not an admin!");
+            if (!args[0]) return message.reply("Please, specify the service you want to restock!");
+            message.reply(`@everyone ${difArg+""+args[0]+""+difArg} has been restocked by '<@${message.author.id}>'`);
         }
     }
-});
-
-bot.login("Token");
+}); bot.login("Token");
